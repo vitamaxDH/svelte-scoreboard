274f21dc757331cc80816eb96a2e5c8d9507d31d
vitamaxDH <hellonewtry@gmail.com>
2020-11-19 23:58:54 +0900

    feat: basic functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,38 @@
 <script>
-	export let name;
+	let name = "John Doe";
+	let points = 100;
+	let showControls = false;
+
+	const addPoint = () => (points += 1);
+	const removePoint = () => (points -= 1);
+	const toggleControls = () => (showControls = !showControls);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		color: #204f6e;
+	}
+	
+	h3 {
+		margin-bottom: 10px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+<div class="container">
+	<div class="card">
+		<h1>
+			{name}
+			<button class="btn btn-sm" on:click="{toggleControls}">
+				{#if showControls}-{:else}+{/if}
+			</button>
+		</h1>
+		<h3>points : {points}</h3>
+		{#if showControls}
+			<button class="btn" on:click="{addPoint}">+1</button>
+			<button class="btn btn-dark" on:click="{removePoint}">-1</button>
+			<input type="number" bind:value="{points}" />
+		{/if}
+	</div>
+</div>
